5713ba30eb95ccad2cf8af938278a2bd27656834
Federico D'Ambrosio <fedexist@gmail.com>
2016-08-31 14:43:12 +0200

    Bump version to 0.1.4, added current maintainers.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 setup(
     name='fp-growth',
     description='A pure-python implementation of the FP-growth algorithm.',
-    version='0.1.3',
+    version='0.1.4',
     author='Eric Naeseth',
     author_email='eric@naeseth.com',
-    url='http://github.com/enaeseth/python-fp-growth/',
-    
+    maintainer='Federico D\'Ambrosio, Edoardo Ferrante',
+    url='http://github.com/fedexist/python-fp-growth/',
     py_modules=['fp_growth']
 )
